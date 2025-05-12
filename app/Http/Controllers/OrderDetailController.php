793a9ec2e8aa82f748c9d20ae1debb57a9165c97
Kruzei <naufal.michiels@gmail.com>
2025-05-12 11:45:00 +0700

    feat(orderdetail): add endpoint to get spesific order ticket

--- app/Http/Controllers/OrderDetailController.php
+++ app/Http/Controllers/OrderDetailController.php
@@ -16,4 +16,21 @@ class OrderDetailController extends Controller
             'data' => $orders
         ]);
     }
+
+    public function show($id)
+    {
+        $order = OrderDetail::with(['user', 'ticketOrders'])->find($id);
+
+        if (!$order) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Order not found'
+            ], 404);
+        }
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $order
+        ]);
+    }
 }
